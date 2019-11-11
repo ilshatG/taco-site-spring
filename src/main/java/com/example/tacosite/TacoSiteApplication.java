f5b0d73abe54f7eb8187555cf06ff2ce492edf04
ilshat <ilshatrashidovich@rambler.ru>
2019-11-11 12:14:24 +0500

    Creating barebone.

---
+++ src/main/java/com/example/tacosite/TacoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacosite/TacoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
